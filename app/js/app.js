1b0e5d423d0b5e19df1ee36ef9e639e20e098f19
test <test@gmail.com>
2023-11-12 18:55:53 +0300

    Заверстал navbar

--- app/js/app.js
+++ app/js/app.js
@@ -1,13 +1,6 @@
 (function($){
 	$(document).ready(function(){ 
 
-		$('.scroll-down').on('click', function(){
-			$('html, body').animate({
-				scrollTop: $('.navbar').offset().top
-			}, 500, function(){ 
-				//console.log('callback function'); 
-			});
-		});
 
 		$(window).on('scroll load', function(e){
 			if(pageYOffset > $('.header').outerHeight(true)){
