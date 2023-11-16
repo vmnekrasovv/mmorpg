f2685afde59101770e0811b86fc71d5cc58071cb
test <test@gmail.com>
2023-11-16 20:18:12 +0300

    Фиксация navbar при скролле

--- app/js/app.js
+++ app/js/app.js
@@ -2,14 +2,31 @@
 	$(document).ready(function(){ 
 
 
+		let prevPos = 0;
+
 		$(window).on('scroll load', function(e){
-			if(pageYOffset > $('.header').outerHeight(true)){
+
+			if((pageYOffset > $('.header').outerHeight(true) + 150) && pageYOffset < prevPos){
+				$('.navbar').removeClass('rollback');
+				$('body').removeClass('navbar-rollback');
+				
 				$('.navbar').addClass('fixed');
 				$('body').addClass('navbar-fixed');
-			} else {
+			}
+
+			else {
+
+				if($('.navbar').hasClass('fixed')){
+					$('.navbar').addClass('rollback');
+					$('body').addClass('navbar-rollback');
+				}
+
 				$('.navbar').removeClass('fixed');
 				$('body').removeClass('navbar-fixed');
 			}
+
+			prevPos = pageYOffset;
+
 		});
 		
 	});
